1a220887465c46d5be20d1b4c761f0c980d877c1
Takuya Aramaki <takuya.aramaki@rakus.co.jp>
2019-06-18 15:53:34 +0900

    Articleフォーム作成

---
+++ src/main/java/com/example/form/ArticleForm.java
@@ -0,0 +1,35 @@
+package com.example.form;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class ArticleForm {
+    /** 投稿者名 */
+    @NotBlank(message = "投稿者名を入力してください")
+    @Size(max = 50, message = "投稿者名は50文字以内で入力してください")
+    private String name;
+    /** 投稿内容 */
+    @NotBlank(message = "投稿内容を入力してください")
+    private String content;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "NewArticleForm [name=" + name + ", content=" + content + "]";
+    }
+}
